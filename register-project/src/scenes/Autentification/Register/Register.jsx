bfafff7c5940ff015bb6958e573cc92f4fd1f9d4
Anna <aniatychenko@gmail.com>
2021-05-16 18:10:00 +0300

    add admin user and toast message

--- register-project/src/scenes/Autentification/Register/Register.jsx
+++ register-project/src/scenes/Autentification/Register/Register.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./Register.module.scss";
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { addUser } from "store/data";
-import { useHistory } from "react-router-dom";
 
+toast.configure();
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -14,42 +15,47 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [address, setAddress] = useState("");
-  let history = useHistory();
-
 
   const handleChangeName = (e) => {
     setName(e.target.value);
-  }
+  };
 
   const handleChangeDate = (e) => {
     setDate(e.target.value);
-  }
+  };
 
   const handleChangeCode = (e) => {
     setCode(e.target.value);
-  }
+  };
 
   const handleChangeSeries = (e) => {
     setSeries(e.target.value);
-  }
+  };
 
   const handleChangePassportNumber = (e) => {
     setPassportNumber(e.target.value);
-  }
+  };
 
   const handleChangeEmail = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const handleChangePassword = (e) => {
     setPassword(e.target.value);
-  }
+  };
 
   const handleChangeAddress = (e) => {
     setAddress(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
+    toast.success("Створено нового реєстратора!", {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      closeButton: false
+    });
     e.preventDefault();
     addUser({
       name,
@@ -59,15 +65,14 @@ const Register = () => {
       passportNumber,
       email,
       password,
-      address
+      address,
     });
-    history.push('/sign-in/')
-  }
+  };
 
   return (
     <div className={styles["form-page-wrapper"]}>
       <form>
-        <h3>Реєстрація</h3>
+        <h3>Реєстрація Реєстратора</h3>
 
         <input
           type="text"
@@ -132,6 +137,7 @@ const Register = () => {
         >
           Зареєструвати
         </button>
+        <ToastContainer />
       </form>
     </div>
   );
